7f11e723a6cab72d0d56e8ac8832ad211c3a9da2
“Magd <majdgalyl@gmail.com>
2022-02-19 02:04:10 +0200

    PokeGame Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
 export class App extends Component {
   render() {
     return (
       <div>
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
+import "./Pokedex.css";
 export class Pokedex extends Component {
-  static defaultProps = {
-    pokeList: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ],
-  };
+  // static defaultProps = {
+  //   pokeList: [
+  //     { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+  //     { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+  //     { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+  //     { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+  //     { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+  //     { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+  //     { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+  //     { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+  //   ],
+  // };
 
   renderList() {
     // const { id, name, type, base_experience } = this.props.pokeList;
@@ -31,7 +32,20 @@ export class Pokedex extends Component {
 
   render() {
     // console.log(this.props.pokeList);
-    return <div>{this.renderList()}</div>;
+    let title;
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+    }
+
+    return (
+      <div>
+        {title}
+        <h4>Total Experience: {this.props.exp}</h4>
+        <div className="Pokedex-container">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
