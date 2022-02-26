969156f5370dce0f144bef77ec22060ecc538183
“Magd <majdgalyl@gmail.com>
2022-02-27 00:07:28 +0200

    RollDice 1.4

--- src/RollDice.js
+++ src/RollDice.js
@@ -5,14 +5,26 @@ import "./RollDice.css";
 export class RollDice extends Component {
   constructor(props) {
     super(props);
-    state = {
-      dsFace: "",
+    this.state = {
+      dsFace: "one",
     };
+    this.roll = this.roll.bind(this);
   }
+
+  roll() {
+    let dice = ["one", "two", "three", "four", "five", "six"];
+    let rand = Math.floor(Math.random() * dice.length);
+
+    this.setState({ dsFace: dice[rand] });
+  }
+
   render() {
+    console.log(this.state.dsFace);
     return (
       <div>
-        <Die dFace={"two"} />
+        <Die dFace={this.state.dsFace} />
+        <Die dFace={this.state.dsFace} />
+        <button onClick={this.roll}>Roll Dice</button>
       </div>
     );
   }
