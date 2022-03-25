1fc4b814242c3b84b111551e37666b13696eac83
“Magd <majdgalyl@gmail.com>
2022-03-25 20:17:19 +0200

    Todo update bug

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,64 @@
 import React, { Component } from "react";
+// import EditTodoForm from "./EditTodoForm";
 
 export class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: false, task: this.props.task };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleRemove(evt) {
+    this.props.removeTodo(this.props.id);
+  }
+
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  handleUpdate(evt) {
+    evt.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: this.toggleForm() });
+  }
+
+  handleChange(evt) {
+    // console.log(evt.target)
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
   render() {
     // console.log(this.props);
-    return (
-      <div>
-        <li>{this.props.task}</li>
-      </div>
-    );
+    let result;
+    if (this.state.isEditing) {
+      // <EditTodoForm />
+      result = (
+        <div>
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              // id={this.props.id}
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            />
+          </form>
+          <button>Save</button>
+        </div>
+      );
+    } else {
+      result = (
+        <div>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleRemove}>X</button>
+          <li>{this.props.task}</li>
+        </div>
+      );
+    }
+    return result;
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import { v4 as uuidv4 } from "uuid";
 import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
+// import EditTodoForm from "./EditTodoForm";
 export class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -9,24 +10,51 @@ export class TodoList extends Component {
       todos: [],
     };
     this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);
   }
 
-  addTodo(todo) {
+  addTodo(newTodo) {
+    this.setState({ todos: [...this.state.todos, newTodo] });
+
     // let newTodo = { ...todo, id: uuidv4() };
     // console.log(newTodo);
-    this.setState({ todos: [...this.state.todos, todo] });
-
     // this.setState((st) => ({
     //   todos: [...st, newTodo],
     // }));
   }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+  }
+
+  updateTodo(id, updatedTask) {
+    const updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, task: updatedTask };
+      }
+      return todo;
+    });
+    this.setState({ todos: updatedTodos });
+  }
+
   render() {
     return (
       <div>
         <h1>TodoList</h1>
         <ul>
           {this.state.todos.map((todo) => {
-            return <Todo key={todo.id} task={todo.task} />;
+            return (
+              <Todo
+                id={todo.id}
+                key={todo.id}
+                task={todo.task}
+                removeTodo={this.removeTodo}
+                updateTodo={this.updateTodo}
+              />
+            );
           })}
         </ul>
         <NewTodoForm addTodo={this.addTodo} />
